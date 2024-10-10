a73666a9b23c45fdb2c29782d48bfe15cb45eba9
Tariq Lashley <tariqlash@gmail.com>
2024-10-09 23:03:42 -0400

    added a greeting to the home screen

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,16 @@ const Home = async () => {
   if (user) redirect("/private/dashboard");
 
   return (
-  <div>
-     <Button className='rounded mr-4 h-12 w-18'><Link href="/login">Log In</Link></Button>
-     <Button className='bg-white hover:bg-gray-300 text-black rounded-full h-12 w-18'><Link href="/register">Sign Up</Link></Button>
+    <div className='flex flex-col justify-center items-center h-full'>
+       <h1 className='text-xl font-bold mb-3'>Hello, Welcome to Locked-In!</h1>
+      <p>Your personal habit tracker designed to help you achieve your goals.</p>
+      <p className='mb-3'>Join our community of habit trackers and start your journey today!</p>
+      <div>
+     
+        <Button className='rounded mr-4 h-12 w-18'><Link href="/login">Log In</Link></Button>
+        <Button className='bg-white hover:bg-gray-300 text-black rounded-full h-12 w-18'><Link href="/register">Sign Up</Link></Button>
+      </div>
+   
   </div>
 
   )
