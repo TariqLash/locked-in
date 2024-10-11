30c695236ff2a7fba59ba48c469563f4806f02be
Tariq Lashley <tariqlash@gmail.com>
2024-10-11 18:00:02 -0400

    db change again

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI!, {
-            useNewUrlParser: true,
             serverSelectionTimeoutMS: 5000, // Adjust this value
             socketTimeoutMS: 45000, // Adjust this value
           })
