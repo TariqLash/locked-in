0b3e3d041f1700483d81e9757783a6c30d49e2ba
Tariq Lashley <tariqlash@gmail.com>
2024-10-11 17:58:58 -0400

    changed mongo connection

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI!)
+        await mongoose.connect(process.env.MONGO_URI!, {
+            useNewUrlParser: true,
+            serverSelectionTimeoutMS: 5000, // Adjust this value
+            socketTimeoutMS: 45000, // Adjust this value
+          })
         console.log(`Successfully connected to mongoDB`)
     } catch (error) {
         {/* @ts-expect-error avoid error */}
